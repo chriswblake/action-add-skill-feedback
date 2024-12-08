7c7557c532cf35147a3ad012aa68918346f1b6d4
Christopher W. Blake <chriswblake@github.com>
2024-12-08 22:22:00 +0000

    Simplify template variable names.

--- index.test.js
+++ index.test.js
@@ -1,7 +1,9 @@
+const github = require('@actions/github');
+
 jest.setTimeout(5 * 60 * 1000);
 
 // Main
-test.skip('runAction - using template name. No variables ', async () => {
+test.skip('runAction - using template name. No variables.', async () => {
     // Context
     // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
     // - Issue is already created in that repo with number 1.
@@ -21,7 +23,7 @@ test.skip('runAction - using template name. No variables ', async () => {
     // Must be manually checked for now
 });
 
-test.skip('runAction - using template name', async () => {
+test.skip('runAction - using template name. No variables. Updated existing.', async () => {
     // Context
     // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
     // - Issue is already created in that repo with number 1.
@@ -32,15 +34,34 @@ test.skip('runAction - using template name', async () => {
     await runAction({
         repoUrl: 'chriswblake/skills-introduction-to-github',
         issueNumber: 1,
+        
+        commentTemplate: 'checking-work',
+        updateRecent: true
+    });
+
+    // Assert
+    // Must be manually checked for now
+});
+
+test.skip('runAction - using template name.', async () => {
+    // Context
+    // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
+    // - Issue is already created in that repo with number 1.
+    // - Comment must already exist for being updated.
+
+    // Arrange
+    
+    // Act
+    await runAction({
+        repoUrl: 'chriswblake/skills-introduction-to-github',
+        issueNumber: 1,
+        updateRecent: false,
 
         commentTemplate: 'lesson-finished',
         commentTemplateVars: JSON.stringify({
-            'github': {
-                'owner': 'chriswblake',
-                'repo': 'introduction-to-github',
-            }
-        }),
-        updateRecent: true
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
+        })
     });
 
     // Assert
@@ -58,14 +79,12 @@ test.skip('runAction - using file path', async () => {
     await runAction({
         repoUrl: 'chriswblake/skills-introduction-to-github',
         issueNumber: 1,
-        updateRecent: true,
+        updateRecent: false,
 
         commentTemplateFile: './templates/lesson-finished.md',
         commentTemplateVars: JSON.stringify({
-            'github': {
-                'owner': 'chriswblake',
-                'repo': 'introduction-to-github',
-            }
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
         })
 
     });
@@ -97,18 +116,16 @@ test('loadCommentFromFile - using template name', () => {
     const content = loadCommentFromFile({
         commentTemplate: 'lesson-finished', 
         commentTemplateVars: {
-            'github': {
-                'owner': 'chriswblake',
-                'repo': 'introduction-to-github',
-            }
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
         }
     });
 
     // Assert
     expect(content).not.toBeNull();
     expect(content).toContain('Congratulations **chriswblake**');
-    expect(content).toContain('chriswblake/introduction-to-github');
-    expect(content).not.toContain('${{ github.repository }}');
+    expect(content).toContain('/chriswblake');
+    expect(content).toContain('introduction-to-github');
 });
 
 test('loadCommentFromFile - using file path', () => {
@@ -118,17 +135,15 @@ test('loadCommentFromFile - using file path', () => {
     const content = loadCommentFromFile({
         commentTemplateFile: './templates/lesson-finished.md', 
         commentTemplateVars: {
-            'github': {
-                'owner': 'chriswblake',
-                'repo': 'introduction-to-github',
-            }
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
         }
     });
 
     // Assert
     expect(content).not.toBeNull();
     expect(content).toContain('Congratulations **chriswblake**');
-    expect(content).toContain('chriswblake/introduction-to-github');
-    expect(content).not.toContain('${{ github.repository }}');
+    expect(content).toContain('/chriswblake');
+    expect(content).toContain('introduction-to-github');
 });
 
