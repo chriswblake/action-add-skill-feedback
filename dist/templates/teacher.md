05b08694f94fe889347125ea59030a11941c5c54
Christopher W. Blake <chriswblake@github.com>
2024-12-08 17:43:25 -0500

    feat: Add template support (#1)

---
+++ dist/templates/teacher.md
@@ -0,0 +1,5 @@
+<img src="https://octodex.github.com/images/Professortocat_v2.png" align="right" height="100px" />
+
+### {{ title }}
+
+{{ body }}
--- index.test.js
+++ index.test.js
@@ -1,16 +1,149 @@
-const { loadCommentFromFile } = require('./index');
-// Methods
-test('loadCommentFromFile - Correctly loads and replaces variables in the markdown file', () => {
-    // Arrange
-    const fileLocation = './comment-premade/x-congratulations.md';
-    process.env.GITHUB_REPOSITORY = 'chriswblake/introduction-to-github-v2';
+const github = require('@actions/github');
 
+jest.setTimeout(5 * 60 * 1000);
+
+// Main
+test.skip('runAction - using template name. No variables.', async () => {
+    // Context
+    // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
+    // - Issue is already created in that repo with number 1.
+
+    // Arrange
+    
     // Act
-    const content = loadCommentFromFile(fileLocation);
+    await runAction({
+        repoUrl: 'chriswblake/skills-introduction-to-github',
+        issueNumber: 1,
+        
+        commentTemplate: 'checking-work',
+        updateRecent: false
+    });
+
+    // Assert
+    // Must be manually checked for now
+});
+
+test.skip('runAction - using template name. No variables. Updated existing.', async () => {
+    // Context
+    // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
+    // - Issue is already created in that repo with number 1.
+
+    // Arrange
+    
+    // Act
+    await runAction({
+        repoUrl: 'chriswblake/skills-introduction-to-github',
+        issueNumber: 1,
+        
+        commentTemplate: 'checking-work',
+        updateRecent: true
+    });
+
+    // Assert
+    // Must be manually checked for now
+});
+
+test.skip('runAction - using template name.', async () => {
+    // Context
+    // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
+    // - Issue is already created in that repo with number 1.
+    // - Comment must already exist for being updated.
+
+    // Arrange
+    
+    // Act
+    await runAction({
+        repoUrl: 'chriswblake/skills-introduction-to-github',
+        issueNumber: 1,
+        updateRecent: false,
+
+        commentTemplate: 'lesson-finished',
+        commentTemplateVars: JSON.stringify({
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
+        })
+    });
+
+    // Assert
+    // Must be manually checked for now
+});
+
+test.skip('runAction - using file path', async () => {
+    // Context
+    // - Environment variable `GITHUB_TOKEN` is set and has access to the repo.
+    // - Issue is already created in that repo with number 1.
+
+    // Arrange
+    
+    // Act
+    await runAction({
+        repoUrl: 'chriswblake/skills-introduction-to-github',
+        issueNumber: 1,
+        updateRecent: false,
+
+        commentTemplateFile: './templates/lesson-finished.md',
+        commentTemplateVars: JSON.stringify({
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
+        })
+
+    });
+
+    // Assert
+    // Must be manually checked for now
+});
+
+
+// Methods
+test('loadCommentFromFile - using template name. No variables', () => {
+    // Arrange
+    
+    // Act
+    const content = loadCommentFromFile({
+        commentTemplate: 'checking-work', 
+    });
 
     // Assert
     expect(content).not.toBeNull();
-    expect(content).toContain('Congratulations');
-    expect(content).toContain('chriswblake/introduction-to-github-v2');
-    expect(content).not.toContain('${{ github.repository }}');
+    expect(content).toContain('inspectocat');
+    expect(content).toContain('Looks like you are making progress');
 });
+
+test('loadCommentFromFile - using template name', () => {
+    // Arrange
+    
+    // Act
+    const content = loadCommentFromFile({
+        commentTemplate: 'lesson-finished', 
+        commentTemplateVars: {
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
+        }
+    });
+
+    // Assert
+    expect(content).not.toBeNull();
+    expect(content).toContain('Congratulations **chriswblake**');
+    expect(content).toContain('/chriswblake');
+    expect(content).toContain('introduction-to-github');
+});
+
+test('loadCommentFromFile - using file path', () => {
+    // Arrange
+    
+    // Act
+    const content = loadCommentFromFile({
+        commentTemplateFile: './templates/lesson-finished.md', 
+        commentTemplateVars: {
+            'login': 'chriswblake',
+            'repo_full_name': 'chriswblake/introduction-to-github'
+        }
+    });
+
+    // Assert
+    expect(content).not.toBeNull();
+    expect(content).toContain('Congratulations **chriswblake**');
+    expect(content).toContain('/chriswblake');
+    expect(content).toContain('introduction-to-github');
+});
+
